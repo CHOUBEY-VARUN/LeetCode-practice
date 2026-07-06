ae29d190a58be813a02652290db5e15b907aa110
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-06 10:11:43 +0530

    07-06-2026 1971-find-if-path-exists-in-graph Depth-First Search Easy

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,34 +1,40 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        createGraph(n,edges,graph);
+        createGraph(n, edges, graph);
         boolean[] visited = new boolean[n];
-        return search(source,destination,graph,visited);
+        return search(source, destination, graph, visited);
     }
 
-    public boolean search(int curr, int dest, ArrayList<ArrayList<Integer>> graph, boolean[] visited){
-        if(curr == dest)return true;
+    public boolean search(int current, int destination, ArrayList<ArrayList<Integer>> graph, boolean[] visited) {
+        if (current == destination) {
+            return true;
+        }
 
-        visited[curr] = true;
-        for(int neighbour : graph.get(curr)){
-            if(!visited[neighbour]){
-                if(search(neighbour,dest,graph,visited))return true;
+        visited[current] = true;
+        for (int neighbour : graph.get(current)) {
+            if (!visited[neighbour]) {
+                if (search(neighbour, destination, graph, visited)) {
+                    return true;
+                }
             }
         }
 
         return false;
+
     }
 
-    public void createGraph(int n, int[][]edges,ArrayList<ArrayList<Integer>> graph){
-        for(int i = 0; i < n; i++){
+    public void createGraph(int n, int[][] edges, ArrayList<ArrayList<Integer>> graph) {
+        for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
-            for(int[] edge : edges){
-                int u = edge[0];
-                int v = edge[1];
 
-                graph.get(u).add(v);
-                graph.get(v).add(u);
-            }
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
     }
 }
