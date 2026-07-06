675a137267ba6ef0b0513b5cd1d11c8f573b69b2
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-06 11:47:11 +0530

    07-06-2026 0547-number-of-provinces Depth-First Search Medium

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,36 +1,22 @@
 class Solution {
     private int provinces = 0;
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        createGraph(isConnected.length, isConnected, graph);
         boolean[] visited = new boolean[isConnected.length];
         for(int i = 0; i<isConnected.length; i++){
             if(!visited[i]){
                 provinces++;
-                search(i,graph,visited);
+                search(i,isConnected,visited);
                 }
         }
         return provinces;
     }
 
-    public void search(int current,ArrayList<ArrayList<Integer>> graph,boolean[] visited){
+    public void search(int current,int[][] isConnected,boolean[] visited){
         visited[current] = true;
 
-        for(int neighbour : graph.get(current)){
-            if(!visited[neighbour]){search(neighbour,graph,visited);}
-        }
-    }
-
-    public void createGraph(int n, int[][] edges, ArrayList<ArrayList<Integer>> graph) {
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0; j<n ; j++){
-                if(edges[i][j] == 1){
-                    graph.get(i).add(j);
-                }
+        for(int neighbour = 0; neighbour < isConnected.length; neighbour++){
+            if(isConnected[current][neighbour] == 1 && !visited[neighbour]){
+                search(neighbour,isConnected,visited);
             }
         }
     }
