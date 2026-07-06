1059563be8a742c77fec7c7a6b664b06f4e00996
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-06 10:53:50 +0530

    07-06-2026 0997-find-the-town-judge Array Easy

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,18 +1,17 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        int [] trustedBy = new int[n+1];
-        int [] trustsOthers = new int[n+1];
+        int [] score = new int[n+1];
 
         for(int[] edge : trust){
             int u = edge[0];
             int v = edge[1];
 
-            trustsOthers[u]++;
-            trustedBy[v]++;
+            score[u]--;
+            score[v]++;
         }
 
         for(int i = 1; i <= n; i++){
-            if(trustsOthers[i] == 0 && trustedBy[i] == n-1)return i;
+            if(score[i] == n-1)return i;
         }
 
         return -1;
