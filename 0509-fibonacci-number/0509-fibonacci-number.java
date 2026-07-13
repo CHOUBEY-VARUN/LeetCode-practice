b7f9ee44a2bc8e301666471785e199880606d8ec
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-13 10:49:08 +0530

    07-13-2026 0509-fibonacci-number Math Easy

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n < 2) return n;
+        Map<Integer,Integer> memo = new HashMap<>();
+        return result(n,memo);
+    }
 
-        return fib(n-2) + fib(n-1);
+    public int result(int n, Map<Integer,Integer> memo){
+        if(n<=1)return n;
+
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int ans = result(n-1,memo) + result(n-2,memo);
+        memo.put(n,ans);
+        return ans;
     }
 }
