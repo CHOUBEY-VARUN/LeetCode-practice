04b92aa49848c113d1cf0a6bb270432d8176ea3c
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-14 11:56:07 +0530

    07-14-2026 0213-house-robber-ii Array Medium

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1)return nums[0];
+        return Math.max(
+            adjSum(nums,1,new HashMap<>(),0),
+            adjSum(nums,0,new HashMap<>(),nums.length-1)
+            );
+    }
+
+    public int adjSum(int[] nums, int n, Map<Integer,Integer> memo,int avoid){
+        if(n >= nums.length || n == avoid) return 0;
+
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int result = Math.max(
+            nums[n] + adjSum(nums,n+2,memo,avoid),
+            adjSum(nums,n+1,memo,avoid)
+        );
+        memo.put(n,result);
+
+        return result;
+    }
+}
