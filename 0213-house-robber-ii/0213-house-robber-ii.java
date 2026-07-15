f0781477bc1c06c3f397094f5feef9b96c7bde09
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-15 09:33:47 +0530

    07-15-2026 0213-house-robber-ii Array Medium

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1)return nums[0];
+        if (nums.length == 1)
+            return nums[0];
+
         return Math.max(
-            adjSum(nums,1,new HashMap<>(),0),
-            adjSum(nums,0,new HashMap<>(),nums.length-1)
-            );
+                adjSum(nums, 0, new HashMap<>(), 0),
+                adjSum(nums, 0, new HashMap<>(), nums.length - 1));
     }
 
-    public int adjSum(int[] nums, int n, Map<Integer,Integer> memo,int avoid){
-        if(n >= nums.length || n == avoid) return 0;
-
-        if(memo.containsKey(n)){
-            return memo.get(n);
+    public int adjSum(int[] nums, int n, Map<Integer, Integer> memo, int avoid) {
+        if (n >= nums.length)
+            return 0;
+        if (n == avoid) {
+            return adjSum(nums, n + 1, memo, avoid);
         }
 
+        if (memo.containsKey(n))
+            return memo.get(n);
+
         int result = Math.max(
-            nums[n] + adjSum(nums,n+2,memo,avoid),
-            adjSum(nums,n+1,memo,avoid)
-        );
-        memo.put(n,result);
+                nums[n] + adjSum(nums, n + 2, memo, avoid),
+                adjSum(nums, n + 1, memo, avoid));
+        memo.put(n, result);
 
         return result;
     }
