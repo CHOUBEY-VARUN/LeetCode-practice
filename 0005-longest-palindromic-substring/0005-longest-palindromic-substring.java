d34e558f08b640d7fdb06b8c2085453c59001a38
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-15 09:59:47 +0530

    07-15-2026 0005-longest-palindromic-substring Two Pointers Medium

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,26 @@
+class Solution {
+    private String result = "";
+    public String longestPalindrome(String s) {
+        for(int i = 0; i<s.length(); i++){
+            search(s,i-1,i+1);
+            search(s,i,i+1);
+            search(s,i,i);
+        }
+
+        return result;
+    }
+
+    public void search(String s, int left, int right){
+        if(left>=0 && right < s.length()){
+            if(s.charAt(left) != s.charAt(right)) return;
+
+            if(result.length() < (right - left +1)){
+                result = s.substring(left,right+1);
+            }
+
+            search(s,left-1,right+1);
+        }
+
+        return; 
+    }
+}
