072ef86db1cf6c7e4e43c21819efdcc9b1ec6bc7
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-22 09:56:41 +0530

    07-22-2026 0322-coin-change Array Medium

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,6 +1,6 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        return numCoins(coins,amount,new HashMap<>());
+        return numCoins(coins,amount, new HashMap<>());
     }
 
     public int numCoins(int[] coins, int amount, Map<Integer,Integer> memo){
@@ -9,13 +9,13 @@ class Solution {
         if(memo.containsKey(amount)) return memo.get(amount);
 
         int min = Integer.MAX_VALUE;
-        
+
         for(int coin : coins){
             int result = numCoins(coins,amount-coin,memo);
-            if(result >= 0) min = Math.min(min,result+1);
+            if(result >= 0) min = Math.min(min,result + 1);
         }
 
-        int ans = min == Integer.MAX_VALUE? -1 : min;
+        int ans = min == Integer.MAX_VALUE ? -1 : min;
         memo.put(amount,ans);
         return ans;
     }
