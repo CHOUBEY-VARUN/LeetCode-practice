989ba8627f486d208a27b7e9f331d2c402dd58e6
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-24 11:31:50 +0530

    07-24-2026 0139-word-break Array Medium

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> words = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[s.length()] = true;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (String word : words) {
+                if ((i + word.length()) <= s.length() &&
+                        (s.startsWith(word, i))) {
+                    dp[i] = dp[i + word.length()];
+                }
+                if (dp[i]) {
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
