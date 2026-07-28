54e68963e3e35f6bfe267f9cc75b0d8464021aa9
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-28 18:44:03 +0530

    07-28-2026 0139-word-break Array Medium

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> words = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[s.length()] = true;
+        boolean[] dict = new boolean[s.length() + 1];
+        dict[s.length()] = true;
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (String word : words) {
-                if ((i + word.length()) <= s.length() &&
-                        (s.startsWith(word, i))) {
-                    dp[i] = dp[i + word.length()];
-                }
-                if (dp[i]) {
-                    break;
+        for(int i = s.length()-1; i>=0; i--){
+            for(String word : words){
+                if(s.startsWith(word,i) && dict[i+word.length()]){
+                    dict[i] = true;
                 }
             }
         }
-        return dp[0];
+        return dict[0];
     }
 }
