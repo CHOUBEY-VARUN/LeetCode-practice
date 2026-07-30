0b214bb054463036f4268cda76fdf42e41a58bc0
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-07-30 19:14:20 +0530

    07-30-2026 0062-unique-paths Math Medium

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return count(0, 0, m, n, dp);
+    }
+
+    public int count(int row, int col, int m, int n, int[][] dp) {
+        if ((row == m - 1) && (col == n - 1))
+            return 1;
+        if (row >= m || col >= n)
+            return 0;
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+
+        int down = count(row+1,col,m,n,dp);
+        int right = count(row,col+1,m,n,dp);
+
+        dp[row][col] = down + right;
+        return dp[row][col];
+    }
+}
