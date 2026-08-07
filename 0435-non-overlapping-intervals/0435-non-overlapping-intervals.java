fa2a92b5e4ee973203b151c9cd6309d1091f20e8
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-08-07 06:52:30 +0530

    08-07-2026 0435-non-overlapping-intervals Array Medium

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,17 +1,16 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        int[] current = intervals[0];
+        Arrays.sort(intervals,(a,b)-> a[0]-b[0]);
         int result = 0;
+        int[] current = intervals[0];
 
         for(int i = 1; i<intervals.length; i++){
             if(intervals[i][0] < current[1]){
                 result++;
-                current = intervals[i][1] <= current[1] ? intervals[i] : current;
-                }
-                else{
-                    current = intervals[i];
-                }
+                current = new int[]{(Math.min(intervals[i][0],current[0])), (Math.min(intervals[i][1],current[1]))};
+            }else{
+                current = intervals[i];
+            }
         }
 
         return result;
