ee98d083a563219c6eee746f18ab1997567f2def
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-08-11 14:55:17 +0530

    08-11-2026 0048-rotate-image Array Medium

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,12 +1,12 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int left = 0;
-        int right = matrix[0].length-1;
         int top = 0;
         int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
 
-        while (left < right) {
-            for (int i = 0; i < right - left; i++) {
+        while(left < right){
+            for(int i = 0; i< right-left; i++){
                 int temp = matrix[top][left + i];
                 matrix[top][left + i] = matrix[bottom - i][left];
                 matrix[bottom - i][left] = matrix[bottom][right - i];
@@ -18,6 +18,5 @@ class Solution {
             top++;
             bottom--;
         }
-
     }
 }
