fb95cfc2f23238aa48ce2960e2f6b2ec407c14d5
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-08-14 12:02:01 +0530

    08-14-2026 0191-number-of-1-bits Divide and Conquer Easy

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,11 +1,9 @@
 class Solution {
     public int hammingWeight(int n) {
         int count = 0;
-        String bin = Integer.toBinaryString(n);
-        for(int i = 0; i<bin.length(); i++){
-            if(bin.charAt(i) == '1'){
-                count++;
-            }
+        while(n != 0){
+            count += n & 1;
+            n >>>= 1;
         }
         return count;
     }
