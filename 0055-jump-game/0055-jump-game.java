1913743122ee4ee7cb0d41771fcf29f4f53bae2b
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-08-22 19:02:41 +0530

    08-22-2026 0055-jump-game Array Medium

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reach = 0;
-
-        for(int i = 0; i<nums.length; i++){
-            if(i > reach) return false;
-            reach = Math.max(reach,i + nums[i]);
-        }
-        return true;
+          int maxReach = nums[0];
+          for(int i = 0; i<nums.length; i++){
+            if(maxReach >= nums.length-1)return true;
+            if(i <= maxReach){
+                maxReach = Math.max(maxReach,(i+nums[i]));
+            }
+          }
+          return false;
     }
 }
