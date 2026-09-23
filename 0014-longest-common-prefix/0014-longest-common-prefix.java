7723fb4f6f93c33ebd1593c2f1e5b67072dce7f9
Varun Choubey <114209466+CHOUBEY-VARUN@users.noreply.github.com>
2026-09-23 09:26:35 +0530

    09-23-2026 0014-longest-common-prefix Array Easy

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,47 +1,16 @@
 class Solution {
-    class TrieNode{
-        TrieNode[] children = new TrieNode[26];
-        boolean isEnd;
-    }
-    private TrieNode root = new TrieNode();
-    
     public String longestCommonPrefix(String[] strs) {
-        StringBuilder prefix = new StringBuilder();
-        TrieNode current = buildTrie(strs,root);
+        StringBuilder prefix = new StringBuilder(strs[0]);
 
-        while(current != null){
-            int childCount = 0;
-            int next = -1;
-
-            for(int i = 0; i<26; i++){
-                if(current.children[i] != null){
-                    childCount++;
-                    next = i; 
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            while (!s.startsWith(prefix.toString())) {
+                prefix.deleteCharAt(prefix.length() - 1);
+                if (prefix.length() == 0) {
+                    return "";
                 }
             }
-
-            if(childCount != 1){break;}
-            if(current.isEnd){break;}
-
-            prefix.append((char)('a'+ next));
-            current = current.children[next];
         }
         return prefix.toString();
-    }
-
-    public TrieNode buildTrie(String[] strs, TrieNode node){
-        TrieNode root = node;
-        for(String str : strs){
-            for(char c : str.toCharArray()){
-                int index = c - 'a';
-                if(node.children[index] == null){
-                    node.children[index] = new TrieNode();
-                }
-                node = node.children[index];
-            }
-            node.isEnd = true;
-            node = root;
-        }
-        return root;
     }
 }
